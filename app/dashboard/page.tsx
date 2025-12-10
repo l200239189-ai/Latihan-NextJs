df3a9018f43d80fc2ad0630fd026f40db20b748e
Limtas223 <taslimanugerah@gmail.com>
2025-12-10 23:40:08 +0700

    Menyelesaikan Bab 8: Fetching Data dan Dynamic Rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,46 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+
+export default async function Page() {
+  // 1. Mengambil data langsung di dalam Komponen Server (Async)
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      
+      {/* 2. Menampilkan Kartu Statistik */}
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+
+      {/* 3. Menampilkan Grafik dan List Invoice Terakhir */}
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* Mengirim data revenue ke komponen Grafik */}
+        <RevenueChart revenue={revenue} />
+        
+        {/* Mengirim data invoice ke komponen List */}
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
